8728e84fe26faf6f86998cc13ec4a202d08cbfd8
Le Duc Son <le.duc.son@sun-asterisk.com>
2020-08-12 21:23:28 +0700

    Layout dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+i = 1
+price = 50
+10.times do |i|
+  i = i + 1
+  price = price + 50
+  Product.create(name: "Product #{i}", price: price)
+end
